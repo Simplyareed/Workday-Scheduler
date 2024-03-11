38e27ee106daf2f2245717cfeaf2f0e675098bcb
Simplyareed <adam.reed1991@gmail.com>
2024-03-11 13:12:03 -0700

    Updating html,css,java, added comments, fixing file path

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,62 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+const localeSettings = {};
+dayjs.locale(localeSettings);
+// Wait until the DOM is fully loaded before executing the code inside the function.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // current hour of the day is being pulled using the dayjs library.
+  const currentHour = dayjs().format('H');
+// The function setHourlyColor changes the color of each time block depending on if it's in the "past, present, or future" pertaining to time.
+function setHourlyColor() {
+  console.log('Hourly Color Function called');
+  const currentHour = parseInt(dayjs().format('H'));
+  // Iterates over each time block, determines if it's in the past, present, or future relative to the current hour,
+// and updates its CSS class to reflect its updated status.
+  $('.time-block').each(function() {
+    const blockHour = parseInt(this.id);
+    const timeClass = blockHour < currentHour ? 'past' : (blockHour === currentHour ? 'present' : 'future');
+    $(this).removeClass('past present future').addClass(timeClass);
+  });
+}
+// The  user's input saved in localStorage, once save button has been clicked.
+  function textEntry() {
+    $('.saveBtn').on('click', function() {
+      const key = $(this).parent().attr('id');
+      const value = $(this).siblings('.description').val();
+      localStorage.setItem(key, value);
+    });
+  }
+ //  function setRefreshColor will refresh the color of each time block based on whether it's in the past,present,or future via the current time. 
+  function setRefreshColor() {
+    $('.time-block').each(function() {
+      const blockHour = parseInt(this.id);
+      if (blockHour == currentHour) {
+        $(this).removeClass('past future').addClass('present');
+      } else if (blockHour < currentHour) {
+        $(this).removeClass('future present').addClass('past');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
+  // user input from the localStorage.
+  $('.time-block').each(function() {
+    const key = $(this).attr('id');
+    const value = localStorage.getItem(key);
+    $(this).children('.description').val(value);
+  });
+
+  
+ // Update the date and time elements in the DOM with the current date and time and displays it 
+  function updateTime() {
+    const dateElement = $('#date');
+    const timeElement = $('#time');
+    const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    dateElement.text(currentDate);
+    timeElement.text(currentTime);
+  }
+  // Call the three main functions to set up the page.
+  setHourlyColor();
+  setRefreshColor();
+  textEntry();                
+  // This will update the time once per second for the current time
+  setInterval(updateTime, 1000);
 });
